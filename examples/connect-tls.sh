ce132822674a391900cf74dedf85448a6241a80b
Martin Grabmueller <martin@grabmueller.de>
2016-06-07 21:24:20 +0200

    Update test skript.

--- examples/connect-tls.sh
+++ examples/connect-tls.sh
@@ -3,5 +3,5 @@
 connect=${1:-127.0.0.1:1234}
 openssl s_client -connect $connect \
 	-CAfile ws1/ca-chain.cert.pem \
-	-cert ws1/6d780e84-d6f3-4824-bcbe-c055ee602039.cert.pem \
-	-key ws1/6d780e84-d6f3-4824-bcbe-c055ee602039.key.pem
+	-cert ws1/1e60b248-ff2e-4dc1-87d5-afe70107b112.cert.pem \
+	-key ws1/1e60b248-ff2e-4dc1-87d5-afe70107b112.key.pem
